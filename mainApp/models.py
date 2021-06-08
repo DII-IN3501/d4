e32c1da517bd15b16dc32d0adb6b5f5e7713fee3
Benjamín Hurtado <benjamin.hurtado@ing.uchile.cl>
2021-06-08 15:37:00 -0400

    Added models

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Persona(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
